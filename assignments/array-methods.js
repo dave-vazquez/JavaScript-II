f89df25b0466a50f1e52586e4ca3f30512c3d5b4
dmvazli <davidmvazquez1@gmail.com>
2019-03-30 11:30:21 -0400

    modified answers, formatted document

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -53,29 +53,87 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":49,"first_name":"Bel","last_name":"Alway","email":"balway1c@ow.ly","shirt_size":"S","company_name":"Voolia","donation":107},
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
 
-// ==== Challenge 1: Use .forEach() ====
-// The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
+
+
+console.log();
+/*********************************************************************************
+*                        CHALLENGE 1: Use .forEach()                             *
+**********************************************************************************/ 
+logHeader('CHALLENGE 1: Use .forEach()', 'Combine both the first and last names into a new array called fullName:');
+
+/* 
+    The event director needs both the first and last names of each runner for their 
+    running bibs.  Combine both the first and last names into a new array called fullName. 
+*/
+
 let fullName = [];
 
 runners.forEach((runner, i) => {
-    fullName[i] = `${runner.first_name} ${runner.last_name}`;
+    fullName.push(`${runner.first_name} ${runner.last_name}`);
 });
+
 console.log(fullName);
 
-// ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
+
+
+
+/*********************************************************************************
+*                           CHALLENGE 2: Use .map()                              *
+**********************************************************************************/ 
+logHeader('CHALLENGE 2: Use .map()', 'Convert each first name into all caps and log the result:');
+
+/* 
+    The event director needs to have all the runner's first names converted to 
+    uppercase because the director BECAME DRUNK WITH POWER. Convert each first name 
+    into all caps and log the result
+*/
+
 let allCaps = runners.map(runner => runner.first_name.toUpperCase());
+
 console.log(allCaps); 
 
-// ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+
+
+
+/*********************************************************************************
+*                           CHALLENGE 3: Use .filter()                           *
+**********************************************************************************/ 
+logHeader('CHALLENGE 3: Use .filter()', 'Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result:');
+
+/*  
+    The large shirts won't be available for the event due to an ordering issue.  
+    Get a list of runners with large sized shirts so they can choose a different size. 
+    Return an array named largeShirts that contains information about the runners that have 
+    a shirt size of L and log the result
+*/
+
 let largeShirts = runners.filter(runner => runner.shirt_size === 'L');
+
 console.log(largeShirts);
 
-// ==== Challenge 4: Use .reduce() ====
-// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
+
+
+
+/*********************************************************************************
+*                           CHALLENGE 4: Use .reduce()                           *
+**********************************************************************************/ 
+logHeader('CHALLENGE 4: Use .reduce()', 'Add up all the donations into a ticketPriceTotal array and log the result:');
+
+/* 
+    The donations need to be tallied up and reported for tax purposes. Add up all 
+    the donations into a ticketPriceTotal array and log the result.
+*/
+
 let ticketPriceTotal = runners.map(runner => runner.donation).reduce((dono1, dono2) => dono1 + dono2);
+
 console.log(ticketPriceTotal);
+
+
+
+
+/*********************************************************************************
+*                             CHALLENGE 5: Be Creative                           *
+**********************************************************************************/ 
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  
@@ -86,3 +144,39 @@ console.log(ticketPriceTotal);
 // Problem 2
 
 // Problem 3
+
+
+
+
+
+
+/* ****************************************************************************************************************************************************************************** */
+
+
+
+
+
+/*********************************************************************************
+*                                   Log Header                                   *
+**********************************************************************************/ 
+
+function logHeader(messageHeader, message = '') {
+
+    (function assembleMessageHeader() {
+        let top    = '\n/*********************************************************************************\n';
+        let middle = `*                                                                                *\n`;
+        let bottom = `**********************************************************************************/\n`;
+
+        let messageStart = (middle.length/2) - (messageHeader.length/2)
+        let messageEnd = (middle.length/2) + (messageHeader.length/2);
+      
+        middle = middle.slice(0,messageStart) + messageHeader + middle.slice(messageEnd, middle.length);
+
+        messageHeader = top + middle + bottom;
+    })();
+    
+  
+    console.log(messageHeader);
+    console.log(message + '\n');
+    console.log('Output:\n');
+  }
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,74 +1,160 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  
-// Study both the problem and the solution to figure out the rest of the problems.
-
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
-/* 
-
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
-
-  // Potential Solution:
-
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
-
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
-
+/*
+  Create a higher order function and invoke the callback function to test your work. 
+  You have been provided an example of a problem and a solution to see how this works with our items array.  
+  Study both the problem and the solution to figure out the rest of the problems.
 */
 
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Notebook', 'yo-yo', 'Gum'];
 
-function getLength(arr, cb) {
-  return cb(arr.length);
+
+console.log(); console.log();
+/*********************************************************************************
+*                                   EXERCISE I                                   *
+**********************************************************************************/ 
+logHeader('EXERCISE 1', 'getLength(items, cb)');
+
+/* getLength passes the length of the array into the callback. */
+
+function getLength(arr, cb) { 
+  cb(arr.length);
 }
 
-getLength(items, length => console.log(length));
+getLength(items, function (length) {     
+  console.log('Output:\n');
+  console.log(length);
+  console.log(); // line break
+});
 
 
+/*********************************************************************************
+*                                   EXERCISE 2                                   *
+**********************************************************************************/ 
+logHeader('EXERCISE 2', 'last(items, cb)');
+
+/* last passes the last item of the array into the callback. */
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-  return cb(arr[arr.length - 1]);
+  cb(arr[arr.length - 1]);
 }
 
-last(items, last => console.log(last));
+last(items, function (lastItem) {
+  console.log('Output:\n');
+  console.log(lastItem);
+  console.log();
+});
+
+/*********************************************************************************
+*                                   EXERCISE 3                                   *
+**********************************************************************************/ 
+logHeader('EXERCISE 3', 'sumNums(2, 4, cb)', false);
+
+/* sumNums adds two numbers (x, y) and passes the result to the callback */
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x + y);
+  cb(x + y);
 }
 
-sumNums(2, 4, sum => console.log(sum));
+sumNums(2, 4, function (sum) {
+  console.log('Output:\n');
+  console.log(sum);
+  console.log();
+});
+
+/*********************************************************************************
+*                                   EXERCISE 4                                   *
+**********************************************************************************/ 
+logHeader('EXERCISE 4', 'multiplyNums(2, 4, cb)', false);
+
+/* multiplyNums multiplies two numbers and passes the result to the callback. */
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x * y);
+  cb(x * y);
 }
 
-multiplyNums(2, 4, product => console.log(product));
+multiplyNums(2, 4, function (product) {
+  console.log('Output:\n');
+  console.log(product);
+  console.log();
+});
+
+/*********************************************************************************
+*                                   EXERCISE 5                                   *
+**********************************************************************************/ 
+logHeader('EXERCISE 5', 'contains(\'yo-yo\', items, cb) ');
+
+/* Contains checks if an item is present inside of the given array/list.
+   Pass true to the callback if it is, otherwise pass false.
+*/
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-
-  return cb(list.includes(item));
+  cb(item, list);
 }
 
-contains('yo-yo', items, listContains => console.log(listContains));
+contains('yo-yo', items, function (item, list) {
+  console.log('Output:\n');
+  console.log(list.includes(item))
+  console.log();
+});
 
-/* STRETCH PROBLEM */
+/*********************************************************************************
+*                                STRETCH PROBLEM                                 *
+**********************************************************************************/ 
+logHeader('STRETCH PROBLEM', 'removeDuplicates(items, cb)');
+
+/* removeDuplicates removes all duplicate values from the given array.
+   Pass the duplicate free array to the callback function. Do not mutate the 
+   original array.
+*/
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+  cb(array.filter((elem, i, arr) => !(arr.indexOf(elem) < i) && !(arr.indexOf(elem) > i)));
+}
+
+removeDuplicates(items, function (uniqueArray) {
+  console.log('Output:\n');
+  console.log(uniqueArray);
+  console.log(); 
+}); 
+
+
+
+
+/* ****************************************************************************************************************************************************************************** */
+
+
+
+
+
+/*********************************************************************************
+*                                 Log Header                                     *
+**********************************************************************************/ 
+
+function logHeader(messageHeader, funcInvoke, showArray = true) {
+
+  (function assembleMessageHeader() {
+    let top    = '/*********************************************************************************\n';
+    let middle = `*                                                                                *\n`;
+    let bottom = `**********************************************************************************/\n`;
+
+    let messageStart = (middle.length/2) - (messageHeader.length/2)
+    let messageEnd = (middle.length/2) + (messageHeader.length/2);
   
-} 
+    middle = middle.slice(0,messageStart) + messageHeader + middle.slice(messageEnd, middle.length);
+
+    messageHeader = top + middle + bottom;
+  })();
+
+  // log header
+  console.log(top + middle + bottom);
+
+  // log array
+  if(showArray) {
+    console.log('const items =  ', items);
+    console.log(); // line break
+  }
+
+  // log function invocation
+  funcInvoke ? console.log(funcInvoke): null;
+  console.log(); // line break
+}
+
